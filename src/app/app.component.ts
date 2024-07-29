e55db431d4b84d3d439a0dc2167e455ff9a29f7e
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-07-29 00:12:24 -0500

    Práctica terminada lista de tareas en angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'lista-tareas-app';
+export class AppComponent implements OnInit {
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  // injección de dependencias
+  private _tareasService = inject(TareasService);
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  eliminarTarea(index: number) {
+    // eliminar tarea por medio de su indice
+    this._tareasService.deleteTarea(index);
+    // llamar a la lista de tareas
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  addTarea() {
+    // guardar tarea en db o local
+    this._tareasService.storeTarea(this.nuevaTarea);
+    // resetear input
+    this.nuevaTarea = '';
+    // llamar a la lista de tareas
+    this.listaTareas = this._tareasService.getTareas();
+  }
 }
